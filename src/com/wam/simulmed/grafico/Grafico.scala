51df7d88ddb2251e08bc9616e71970f94f2b2f68
jportegame <jportegame@gmail.com>
2019-08-02 19:06:44 -0500

    Se suben las clases que se encargan de la parte grafica con JFreeChart ademas del KeyListen que controla el flujo con las teclas F5 y F6

---
+++ src/com/wam/simulmed/grafico/Grafico.scala
@@ -0,0 +1,166 @@
+package com.wam.simulmed.grafico
+
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.Map
+import com.wam.simulmed.ciudad._
+import com.wam.simulmed.inmovil._
+import com.wam.simulmed.movil._
+//Libreria para frames
+import javax.swing.JFrame
+//Libreria para lineas
+import java.awt.BasicStroke;
+//Libreria para colores
+import java.awt.Color;
+//JfreeChart
+import org.jfree.chart.ChartFrame
+import org.jfree.chart.ChartFactory
+import org.jfree.chart.JFreeChart
+import org.jfree.chart.plot.PlotOrientation
+import org.jfree.chart.plot.XYPlot
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer
+import org.jfree.data.xy.XYSeries
+import org.jfree.data.xy.XYSeriesCollection
+import org.jfree.chart.ChartFrame
+import org.jfree.chart.axis.ValueAxis
+import org.jfree.chart.annotations.XYTextAnnotation
+import java.awt.event.KeyListener
+
+import java.util.Random
+
+//Figuras geometricas
+import java.awt.geom.Ellipse2D
+import java.awt.Rectangle
+import java.awt.Polygon
+
+object Grafico{
+  
+  val coloresIntersecciones:Map[Interseccion, String]=Map()
+  val dataset: XYSeriesCollection = new XYSeriesCollection();
+  val renderer: XYLineAndShapeRenderer = new XYLineAndShapeRenderer()
+  var plot: XYPlot = null
+  var ventana: ChartFrame = null;
+  
+  var punto1: XYSeries = new XYSeries("1")
+  punto1.add(0, 0)
+  dataset.addSeries(punto1)
+  
+  def iniciarGrafico(vias:ArrayBuffer[Via]){
+  	
+    this.renderer.setAutoPopulateSeriesStroke(false)
+  	this.renderer.setAutoPopulateSeriesPaint(false)
+  	
+    this.renderer.setBaseStroke(new BasicStroke(4))
+    this.renderer.setBasePaint(Color.decode("#cccccc"))
+    
+    cargarMapa(vias)
+    
+    val xyScatterChart: JFreeChart = ChartFactory.createScatterPlot(
+  	null, 
+  	null, 
+  	null, 
+  	this.dataset,
+  	PlotOrientation.VERTICAL, false, false, false)
+
+
+  	this.plot=xyScatterChart.getXYPlot()
+
+  	this.plot.setBackgroundPaint(Color.WHITE)
+  	
+
+  	val range:  ValueAxis = plot.getRangeAxis()
+    range.setVisible(false)
+    val domain: ValueAxis = plot.getDomainAxis()
+    domain.setVisible(false)
+    
+    this.cargarIntersecciones(plot)
+
+  	this.plot.setRenderer(this.renderer)
+  	
+
+  	this.ventana = new ChartFrame("vehTraffic", xyScatterChart)
+  	ventana.setVisible(true)
+  	ventana.setSize(1300, 700)
+  	ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+  	ventana.addKeyListener(KeyListen)
+  }
+  
+  def cargarMapa(vias:ArrayBuffer[Via]){
+    vias.foreach(via=>{
+      val nuevaVia: XYSeries  = new XYSeries(via.nombre+"-"+via.origen.nombre+"-"+via.fin.nombre)
+    	nuevaVia.add(via.origen.xi, via.origen.yi)
+    	nuevaVia.add(via.fin.xi, via.fin.yi)
+      this.dataset.addSeries(nuevaVia)
+      renderer.setSeriesShapesVisible(dataset.getSeriesCount-1, false)
+    })
+  }
+  
+  def borrarVehiculos(vehiculosSimulacion: ArrayBuffer[VehiculoSimulacion]) = {
+    vehiculosSimulacion.foreach(vehiculoSimulacion => {
+      dataset.removeSeries(dataset.getSeriesIndex(vehiculoSimulacion.vehiculo.placa))
+    })
+  }
+  
+  def cargarIntersecciones(plot:XYPlot){
+    val random = new Random()
+    val intersecciones:ArrayBuffer[Interseccion]=GrafoVias.listaDeNodos
+    intersecciones.foreach(interseccion=>{
+      val colorHex=randomHex()
+      this.renderer.setSeriesPaint(this.dataset.getSeriesCount,Color.decode(colorHex))
+      val label: XYTextAnnotation = new XYTextAnnotation(interseccion.nombre,interseccion.xi, interseccion.yi+(350)*((random.nextFloat()*2).round-1))
+    	label.setPaint(Color.decode(colorHex))
+    	plot.addAnnotation(label)
+    	this.coloresIntersecciones+=(interseccion->colorHex)
+    })
+  }
+  
+  
+  def cargarVehiculo(vehiculoSimulacion:VehiculoSimulacion){
+    val vehiculo=vehiculoSimulacion.vehiculo
+    val punto=vehiculo.posicion
+    val vehiculoGrafico: XYSeries = new XYSeries(vehiculo.placa)
+    vehiculoGrafico.add(punto.x, punto.y)
+    this.dataset.addSeries(vehiculoGrafico)
+    vehiculo match{
+      case vehiculo:Bus=>{
+        this.renderer.setSeriesShape(this.dataset.getSeriesCount-1, new Polygon(Array(-3,3,5,0,-5),Array(-6,-6,0,4,0),5))
+        this.renderer.setSeriesPaint(this.dataset.getSeriesCount-1,Color.decode(this.coloresIntersecciones(vehiculoSimulacion.interseccionesCompletas.last)))
+      }
+      case vehiculo:Camion=>{
+        this.renderer.setSeriesShape(this.dataset.getSeriesCount-1, new Rectangle(-2,-4,4,10))
+        this.renderer.setSeriesPaint(this.dataset.getSeriesCount-1,Color.decode(this.coloresIntersecciones(vehiculoSimulacion.interseccionesCompletas.last)))
+      }
+      case vehiculo:Carro=>{
+        this.renderer.setSeriesShape(this.dataset.getSeriesCount-1, new Ellipse2D.Double(-4,-4,8,10))
+        this.renderer.setSeriesPaint(this.dataset.getSeriesCount-1,Color.decode(this.coloresIntersecciones(vehiculoSimulacion.interseccionesCompletas.last)))
+      }
+      case vehiculo:Moto=>{
+        this.renderer.setSeriesShape(this.dataset.getSeriesCount-1, new Polygon(Array(-4, 0, +4, 0),Array(4, 2, 4, -4),4))
+        this.renderer.setSeriesPaint(this.dataset.getSeriesCount-1,Color.decode(this.coloresIntersecciones(vehiculoSimulacion.interseccionesCompletas.last)))
+      }
+      case vehiculo:MotoTaxi=>{
+        this.renderer.setSeriesShape(this.dataset.getSeriesCount-1, new Polygon(Array(-5,0,5),Array(-5,5,-5),3))
+        this.renderer.setSeriesPaint(this.dataset.getSeriesCount-1,Color.decode(this.coloresIntersecciones(vehiculoSimulacion.interseccionesCompletas.last)))
+      }
+    }
+    
+  }
+  
+  def actualizarVehiculo(vehiculoSimulacion:VehiculoSimulacion){
+    val vehiculo=vehiculoSimulacion.vehiculo
+    val punto=vehiculo.posicion
+    val vehiculoGrafico: XYSeries = this.dataset.getSeries(vehiculo.placa)
+    vehiculoGrafico.clear()
+    vehiculoGrafico.add(punto.x, punto.y)
+  }
+  
+  def randomHex():String={
+      val random = new Random()
+      var letters:Array[String] = "0123456789ABCDEF".split("")
+      var color = "#"
+      for (i<- 0 to 6) {
+        val index:Int=((random.nextFloat()*15).round)
+        color=color+letters(index)
+      }
+      color
+  }
+}
---
+++ src/com/wam/simulmed/grafico/KeyListen.scala
@@ -0,0 +1,28 @@
+package com.wam.simulmed.grafico
+
+import java.awt.event.KeyListener
+import java.awt.event.WindowEvent
+import java.awt.event.KeyEvent
+import com.wam.simulmed.inmovil.Simulacion
+
+object KeyListen extends KeyListener {
+  def keyPressed(event: KeyEvent) {
+
+    if (event.getKeyCode == 116) {
+
+        Simulacion.start()
+      
+    }
+
+    if (event.getKeyCode == 117) {
+      Simulacion.stop()
+    }
+
+  }
+
+  def keyReleased(event: KeyEvent) {
+  }
+
+  def keyTyped(event: KeyEvent) {
+  }
+}
