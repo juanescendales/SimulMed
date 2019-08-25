a0527ef8a68ba442b8b40c10a0a99ec894e4005b
jportegame <jportegame@unal.edu.co>
2019-08-25 18:45:30 -0500

    Correccion merge

--- src/com/wam/simulmed/inmovil/Recta.scala
+++ src/com/wam/simulmed/inmovil/Recta.scala
@@ -4,5 +4,5 @@ trait Recta {
   type T<: Punto
   val origen:T
   val fin:T
-  val angulo =  math.atan2(fin.y - origen.y, fin.x - origen.x).toDegrees
+  val angulo =  math.atan(math.abs(fin.y - origen.y) / math.abs(fin.x - origen.x)).toDegrees 
 }
