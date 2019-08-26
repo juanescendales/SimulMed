7fc6cd53d832169e36df20edefcca80e91187143
Juanma <jpajoyl@unal.edu.co>
2019-08-26 11:23:38 -0500

    sol merge

--- src/com/wam/simulmed/ciudad/Comparendo.scala
+++ src/com/wam/simulmed/ciudad/Comparendo.scala
@@ -1,10 +1,14 @@
 package com.wam.simulmed.ciudad
 import com.wam.simulmed.movil._
+import scala.collection.mutable.ArrayBuffer
 class Comparendo(val vehiculo: Vehiculo, val velocidadVia: Double) {
-
+  
   val velocidadVehiculo: Double = vehiculo.velocidad.velocidadTotalMagnitud
 
   def porcentajeExcedido: Double = {
     ((velocidadVehiculo/velocidadVia)-1)*100
   }
+}
+object Comparendo{
+  val listaComparendos = new ArrayBuffer[Comparendo]
 }
