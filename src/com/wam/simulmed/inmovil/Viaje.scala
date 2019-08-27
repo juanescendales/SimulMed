8ba033df8003b6cb8fe31c17b981975cabfc7b22
jsapuyesp <jsapuyesp@unal.edu.co>
2019-08-27 15:38:58 -0500

    correccion aceleracion

--- src/com/wam/simulmed/inmovil/Viaje.scala
+++ src/com/wam/simulmed/inmovil/Viaje.scala
@@ -89,6 +89,7 @@ class Viaje(val vehiculo: Vehiculo, val recorrido: Queue[Via], val interseccione
 
       }
       //println(vehiculo.aceleracion.magnitud)
+      println(vehiculo.velocidad.magnitud)
     }
   }
 
