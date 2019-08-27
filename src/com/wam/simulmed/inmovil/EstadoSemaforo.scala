9e4ad887b0523a5b3251a11d6446854d5691863f
juanescendales <jcendaless@unal.edu.co>
2019-08-27 16:42:13 -0500

    estado semaforo en neo4j

--- src/com/wam/simulmed/inmovil/EstadoSemaforo.scala
+++ src/com/wam/simulmed/inmovil/EstadoSemaforo.scala
@@ -15,16 +15,19 @@ object EstadoAmarillo extends EstadoSemaforo{
   private val tiempoAmarillo:Double=15
   val distanciaFrenado:Double=100
   def getTiempo:Double=tiempoAmarillo
+  override def toString = "EstadoAmarillo"
 } 
 
 object EstadoVerde extends EstadoSemaforo{
   val tiempoVerde:Double=120
   val distanciaFrenado:Double=EstadoRojo.distanciaFrenado*2
   def getTiempo:Double=tiempoVerde
+  override def toString = "EstadoVerde"
 }
 
 object EstadoRojo extends EstadoSemaforo{
   val tiempoRojo:Double=120
   val distanciaFrenado:Double=200
   def getTiempo:Double=tiempoRojo
+  override def toString = "EstadoRojo"
 }
