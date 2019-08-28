9bab2274aba8f5f2058f563b8555d71ce18042b7
juanescendales <jcendaless@unal.edu.co>
2019-08-27 21:59:21 -0500

    adelanto correccion

--- src/com/wam/simulmed/ciudad/Interseccion.scala
+++ src/com/wam/simulmed/ciudad/Interseccion.scala
@@ -2,4 +2,10 @@ package com.wam.simulmed.ciudad
 
 import com.wam.simulmed.inmovil.Punto
 import scala.collection.mutable.ArrayBuffer
-case class Interseccion(xi: Int, yi: Int, val nombre: Option[String]) extends Punto(xi, yi)
+import scala.collection.mutable.Map
+case class Interseccion(xi: Int, yi: Int, val nombre: Option[String]) extends Punto(xi, yi) {
+  Interseccion.listaIntersecciones += ((this.nombre.getOrElse("")-> this))
+}
+object Interseccion {
+  val listaIntersecciones = collection.mutable.Map[String, Interseccion]()
+}
--- src/com/wam/simulmed/inmovil/Conexion.scala
+++ src/com/wam/simulmed/inmovil/Conexion.scala
@@ -1,13 +1,46 @@
 package com.wam.simulmed.inmovil
 import org.neo4j.driver.v1._
+import com.wam.simulmed.ciudad._
 object Conexion {
   val url = "bolt://localhost/7687"
-  val user = "neo4j" 
-  val pass = "1234" 
+  val user = "neo4j"
+  val pass = "1234"
+
+  //RECORDAR QUE HAY QUE METER LAS SIGUIENTES 2 LINEAS PARA CERRAR CONEXION Y ESO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  //session.close()
+  //driver.close()
 
   def getSession(): (Driver, Session) = {
     val driver = GraphDatabase.driver(url, AuthTokens.basic(user, pass))
     val session = driver.session
     (driver, session)
+  }
+  def getInterseccion(nombre: String): Option[Interseccion] = {
+    val (driver, session) = getSession()
+    val script = s"match(n:Interseccion{nombre:'$nombre'})return(n)"
+    val result = session.run(script)
+    val interseccion =
+      if (result.hasNext()) {
+        val nodo = result.next().values().get(0)
+        Some(new Interseccion(nodo.get("xi").asInt(), nodo.get("yi").asInt(), Some(nodo.get("nombre").asString())))
+      } else {
+        None
+      }
+    interseccion
+  }
+
+  def getVias: Array[Option[Via]] = {
+    //match(io:Interseccion)-[:ES_ORIGEN]->(v:Via)<-[:ES_FIN]-(if:Interseccion)
+    val (driver, session) = getSession()
+    val script = s"match(io:Interseccion)-[:ES_ORIGEN]->(v:Via)<-[:ES_FIN]-(if:Interseccion)"
+    val result = session.run(script)
+    val interseccion =
+      if (result.hasNext()) {
+        val nodo = result.next().values().get(0)
+        Some(new Interseccion(nodo.get("xi").asInt(), nodo.get("yi").asInt(), Some(nodo.get("nombre").asString())))
+      } else {
+        None
+      }
+    interseccion
   }
 }
