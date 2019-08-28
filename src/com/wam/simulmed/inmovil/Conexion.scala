56d7c2abca5f270dc08bc42718f8cbca8544cc87
juanescendales <jcendaless@unal.edu.co>
2019-08-27 22:43:40 -0500

    adelanto conexion

--- src/com/wam/simulmed/inmovil/Conexion.scala
+++ src/com/wam/simulmed/inmovil/Conexion.scala
@@ -1,6 +1,7 @@
 package com.wam.simulmed.inmovil
 import org.neo4j.driver.v1._
 import com.wam.simulmed.ciudad._
+import scala.collection.mutable.ArrayBuffer
 object Conexion {
   val url = "bolt://localhost/7687"
   val user = "neo4j"
@@ -28,11 +29,18 @@ object Conexion {
       }
     interseccion
   }
-
-  def getVias: Array[Option[Via]] = {
-    //match(io:Interseccion)-[:ES_ORIGEN]->(v:Via)<-[:ES_FIN]-(if:Interseccion)
+  def getIntersecciones {
     val (driver, session) = getSession()
-    val script = s"match(io:Interseccion)-[:ES_ORIGEN]->(v:Via)<-[:ES_FIN]-(if:Interseccion)"
+    val script = "match(n:Interseccion)return(n)"
+    val result = session.run(script)
+    while (result.hasNext()) {
+      val node = result.next().values().get(0)
+      getInterseccion(node.get("nombre").asString())
+    }
+  }
+  def getCamaraPorIdVia(id: Int): Option[CamaraFotoDeteccion]={
+    val (driver, session) = getSession()
+    val script = s"match(n:Interseccion{nombre:'$nombre'})return(n)"
     val result = session.run(script)
     val interseccion =
       if (result.hasNext()) {
@@ -42,5 +50,24 @@ object Conexion {
         None
       }
     interseccion
+  }
+
+  def getVias: ArrayBuffer[Via] = {
+    //match(io:Interseccion)-[:ES_ORIGEN]->(v:Via)<-[:ES_FIN]-(if:Interseccion)
+    val (driver, session) = getSession()
+    val script = s"match(io:Interseccion)-[:ES_ORIGEN]->(v:Via)<-[:ES_FIN]-(if:Interseccion) return io,v,if"
+    val result = session.run(script)
+    val arrayVias = ArrayBuffer[Via]()
+    val interseccion =
+      while(result.hasNext()) {
+        val values = result.next().values()
+        val NodoIo = values.get(0)
+        val NodoV = values.get(1)
+        val NodoIf = values.get(2)
+        val interseccionInicio = Interseccion.listaIntersecciones.get(NodoIo.get("nombre").asString()).get
+        val interseccionFinal = Interseccion.listaIntersecciones.get(NodoIf.get("nombre").asString()).get
+        arrayVias += new Via(NodoV.get("id").asInt(), interseccionInicio, interseccionFinal)
+      }
+    arrayVias
   }
 }
